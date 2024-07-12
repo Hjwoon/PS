b0f368e277a7f04b4e733481d29373b69b2e84e3
Hjwoon <hjw030705@naver.com>
2024-07-12 10:31:35 +0900

    ✨ #7287

---
+++ PS/src/s7287.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class s7287 { // 백준에서 제출 시 컴파일 에러 -> Main으로 변경
+    public static void main(String[] args) throws IOException {
+        System.out.print("65\nagn705");
+    }
+}
