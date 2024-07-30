b325b1d643faf8f80fe40223c27fa7c280c71ce1
Hjwoon <100463930+Hjwoon@users.noreply.github.com>
2024-07-30 17:13:02 +0900

    ✨ #2393

---
+++ PS/src/s2393.java
@@ -0,0 +1,22 @@
+public class s2393 {
+
+    public void print() {
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    }
+
+    public static void main(String[] args) {
+        new s2393().print();
+    }
+}
