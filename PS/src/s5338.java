d76fd9854377d353a21d6cc9b473617e363ae890
Hjwoon <100463930+Hjwoon@users.noreply.github.com>
2024-08-08 22:37:44 +0900

    ✨ #5338

---
+++ PS/src/s5338.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class s5338 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
